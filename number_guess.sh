98240fa1294ab0f4bda1a4ab3809c27a3086cfb0
1001Rem <user.no.1@tuta.com>
2024-12-28 15:10:39 +0000

    feat: get username

--- number_guess.sh
+++ number_guess.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+#random no gen
 R_NO=$(($RANDOM%1000))
 
-
+#get username
 echo Enter your username:
 read USERNAME
+
