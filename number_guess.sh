0fc8b8923c1ad8770def810edcae8235b44e326b
1001Rem <user.no.1@tuta.com>
2024-12-28 15:22:34 +0000

    feat: added user num input

--- number_guess.sh
+++ number_guess.sh
@@ -9,3 +9,22 @@ R_NO=$(($RANDOM%1000))
 echo Enter your username:
 read USERNAME
 
+QUERY=$($PSQL "SELECT * FROM gamers WHERE username = '$USERNAME'")
+
+if [[ $NAME != '' ]]
+then
+
+echo "$QUERY" | while read USER BAR GAMES_PLAYED BAR BEST_GAME
+do
+
+echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+
+done
+
+else 
+
+echo "Welcome, $USERNAME! It looks like this is your first time here."
+fi
+
+echo "Guess the secret number between 1 and 1000:"
+read INPUT
