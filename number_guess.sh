cdffd171c4e7496a2f89ad26cee4078504558b15
1001Rem <user.no.1@tuta.com>
2024-12-29 12:05:29 +0000

    fix: unit tests not passing on no.8

--- number_guess.sh
+++ number_guess.sh
@@ -1,36 +1,97 @@
 #!/bin/bash
 
+#query DB
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
-#random no gen
-R_NO=$(($RANDOM%1000))
+#generate random number 1 - 1000
+R_NO=$(( $RANDOM % 1000 ))
 
-#get username
-echo Enter your username:
-read USERNAME
+#tries count var
+TRIES=0
 
-QUERY=$($PSQL "SELECT * FROM gamers WHERE username = '$USERNAME'")
+#is new player?
+ISNEW=true
 
-if [[ $NAME != '' ]]
-then
+#get username input
+echo -e "\nEnter your username:"
+read NAME
 
-echo "$QUERY" | while read USER BAR GAMES_PLAYED BAR BEST_GAME
-do
+#see if username already exist 
+QUERY=$($PSQL "SELECT * FROM users WHERE username ='$NAME'")
 
-echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+#check if username exist
+case $QUERY in
 
-done
+'') echo -e "\nWelcome, $NAME! It looks like this is your first time here.";;
 
-else 
 
-echo "Welcome, $USERNAME! It looks like this is your first time here."
-fi
+*) IFS='|' read -r USERNAME GAMES_PLAYED BEST_GAME <<< "$QUERY"
 
-echo "Guess the secret number between 1 and 1000:"
+echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+
+#set new user flag to false
+ISNEW=false;;
+esac
+
+#prompt for int input and read 
+echo -e "\nGuess the secret number between 1 and 1000:"
 read INPUT
 
 while [[ ! $INPUT =~ ^[0-9]+$ ]]
 do
-echo "That is not an integer, guess again:"
+echo -e "\nThat is not an integer, guess again:"
 read INPUT
 done
+
+#count first try after provided int 
+((TRIES++))
+
+#check if input is less than or more than the random number
+while (( $INPUT != $R_NO ))
+do
+
+if [[ $INPUT -gt $R_NO ]]
+then
+((TRIES++))
+echo -e "\nIt's lower than that, guess again:"
+read INPUT
+
+else
+
+((TRIES++))
+echo -e "\nIt's higher than that, guess again:"
+read INPUT
+
+fi
+
+done
+
+#print win message when loop is broken 
+echo -e "\nYou guessed it in $TRIES tries. The secret number was $R_NO. Nice job!"
+
+
+# #if player is new
+if [[ $ISNEW = 'true' ]]
+then
+#insert current first game stats
+($PSQL "INSERT INTO users(username, games_played, best_game) VALUES('$NAME', 1, $TRIES)") > /dev/null
+
+fi
+
+#if player is not new 
+if [[ $ISNEW = 'false' ]]
+then
+
+($PSQL "UPDATE users SET games_played = games_played + 1 WHERE username = '$NAME'") > /dev/null
+
+BEST_GAMES=$($PSQL "SELECT best_game FROM users WHERE username = '$NAME'")
+
+if [[ $TRIES -lt $BEST_GAMES ]]
+then
+
+($PSQL "UPDATE users SET best_game = $TRIES WHERE username = '$NAME' ") > /dev/null
+
+fi
+
+fi
+
